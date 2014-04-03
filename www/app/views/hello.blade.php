4658341e5b38ce0cbe9f3d487786d88ad0402691
Douglas Shaffren <dshaffren@gmail.com>
2014-04-02 20:03:38 -0400

    Update hello.blade.php
    updates title

--- www/app/views/hello.blade.php
+++ www/app/views/hello.blade.php
@@ -1,8 +1,8 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Laravel PHP Framework</title>
+    <meta charset="UTF-8">
+    <title>Voting Results</title>
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
